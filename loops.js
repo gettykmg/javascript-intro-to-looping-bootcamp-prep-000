449e91d73e9aadbd2cb64f03eee87d44a255fb7a
Keith Getty <keithgetty@yahoo.com>
2017-09-17 19:56:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,8 +17,7 @@ function whileLoop(n) {
 }
 
 function maybeTrue() {
-   return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1
- (exclusive)
+   return Math.random() >= 0.5 
  }
 
 function doWhileLoop(array) {
